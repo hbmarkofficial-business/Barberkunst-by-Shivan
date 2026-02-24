6c01faee73caf633aa3444260b38f93bb4ce5d1a
hbmarkofficial-business <hbmark.official@gmail.com>
2026-02-24 17:47:44 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -12,63 +12,63 @@ const Index = () => {
     <>
       <Helmet>
         {/* Titel & Meta Description */}
-        <title>Barbershop Prenzlauer Berg | The Gentleman's Cut</title>
+        <title>Barbershop Bonn | Barberkunst by Shivan</title>
         <meta
           name="description"
-          content="Besuche unseren Premium Barbershop The Gentleman's Cut in Berlin Prenzlauer Berg für erstklassige Haarschnitte, Bartpflege und individuelle Styles. Jetzt Termin buchen!"
+          content="Besuche unseren Premium Barbershop Barberkunst by Shivan in Bonn für erstklassige Haarschnitte, Bartpflege und individuelle Styles. Jetzt Termin buchen!"
         />
         <meta
           name="keywords"
-          content="Barbershop, Herrenfriseur, Haarschnitt, Bartpflege, Rasur, Berlin, Prenzlauer Berg"
+          content="Barbershop, Herrenfriseur, Haarschnitt, Bartpflege, Rasur, Bonn, Barberkunst"
         />
-        <link rel="canonical" href="https://gentlemanscut.de" />
+        <link rel="canonical" href="https://barberkunst-shivan.de" />
 
         {/* Open Graph */}
-        <meta property="og:title" content="The Gentleman's Cut | Premium Barbershop Berlin" />
+        <meta property="og:title" content="Barberkunst by Shivan | Premium Barbershop Bonn" />
         <meta
           property="og:description"
-          content="Erstklassige Haarschnitte und Bartpflege für den modernen Gentleman in Berlin Prenzlauer Berg."
+          content="Erstklassige Haarschnitte und Bartpflege für den modernen Gentleman in Bonn."
         />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://gentlemanscut.de" />
-        <meta property="og:image" content="https://gentlemanscut.de/images/og-image.png" />
+        <meta property="og:url" content="https://barberkunst-shivan.de" />
+        <meta property="og:image" content="https://barberkunst-shivan.de/images/og-image.png" />
 
         {/* Twitter Card */}
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="The Gentleman's Cut | Premium Barbershop Berlin" />
+        <meta name="twitter:title" content="Barberkunst by Shivan | Premium Barbershop Bonn" />
         <meta
           name="twitter:description"
           content="Erleben Sie erstklassige Haarschnitte und Bartpflege für den modernen Gentleman."
         />
-        <meta name="twitter:image" content="https://gentlemanscut.de/images/og-image.png" />
+        <meta name="twitter:image" content="https://barberkunst-shivan.de/images/og-image.png" />
 
         {/* Schema.org LocalBusiness */}
         <script type="application/ld+json">
           {JSON.stringify({
             "@context": "https://schema.org",
             "@type": "Barbershop",
-            "name": "The Gentleman's Cut",
-            "image": "https://gentlemanscut.de/images/logo.png",
+            "name": "Barberkunst by Shivan",
+            "image": "https://barberkunst-shivan.de/images/logo.png",
             "address": {
               "@type": "PostalAddress",
-              "streetAddress": "Straße 12",
-              "addressLocality": "Berlin",
-              "postalCode": "10115",
+              "streetAddress": "Maxstraße 2",
+              "addressLocality": "Bonn",
+              "postalCode": "53111",
               "addressCountry": "DE"
             },
             "geo": {
               "@type": "GeoCoordinates",
-              "latitude": "52.5200",
-              "longitude": "13.4050"
+              "latitude": "50.7374",
+              "longitude": "7.0982"
             },
-            "url": "https://gentlemanscut.de",
-            "telephone": "+49 30 1234567",
-            "openingHours": "Mo-Fr 09:00-19:00, Sa 09:00-17:00",
+            "url": "https://barberkunst-shivan.de",
+            "telephone": "+49 228 42225577",
+            "openingHours": "Mo-Fr 09:00-19:00, Sa 09:00-18:00",
             "priceRange": "€€",
             "servesCuisine": "Barbershop Services",
             "sameAs": [
-              "https://www.facebook.com/gentlemanscut",
-              "https://www.instagram.com/gentlemanscut"
+              "https://www.facebook.com/barberkunstbyshivan",
+              "https://www.instagram.com/barberkunstbyshivan"
             ]
           })}
         </script>
@@ -91,4 +91,3 @@ const Index = () => {
 };
 
 export default Index;
-
